2317195b6c8f37490fbc8a7055c40cf0feb4e101
Carlos Ferreira <carlos.ferreira@decode.pt>
2021-02-11 18:00:41 +0000

    ADD services

---
+++ Next.Steps.Domain/Interfaces/Services/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using Next.Steps.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Next.Steps.Domain.Interfaces.Services
+{
+    interface IPersonService
+    {
+        void Create(Person p);
+        void Update(Person p);
+        void Delete(int id);
+        Person GetByID(int id);
+        IEnumerable<Person> GetAll();
+        IEnumerable<Person> Search(string firstName, string lastName = "");
+
+    }
+}
---
+++ Next.Steps.Domain/Services/PersonService.cs
@@ -0,0 +1,40 @@
+﻿using Next.Steps.Domain.Entities;
+using Next.Steps.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+
+namespace Next.Steps.Domain.Services
+{
+    public class PersonService : IPersonService
+    {
+        public void Create(Person p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> Search(string firstName, string lastName = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Person p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
