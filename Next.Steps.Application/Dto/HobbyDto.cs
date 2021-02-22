1e5596717188e6ec79221a130dda2a6bf04c5e14
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-22 18:08:34 +0000

    Create Dtos

---
+++ Next.Steps.Application/Dto/HobbyDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Next.Steps.Application.Dto
+{
+    public class HobbyDto
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+    }
+}
---
+++ Next.Steps.Application/Dto/PersonDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Next.Steps.Application.Dto
+{
+    public class PersonDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Person's FirstName is Required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Person's LastName is Required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Person's Profession is Required")]
+        public string Profession { get; set; }
+
+        [Required(ErrorMessage = "Person's Birthday is Required")]
+        public DateTime Birthdate { get; set; }
+
+        [Required(ErrorMessage = "Person's Email is Required")]
+        public string Email { get; set; }
+
+        public virtual IEnumerable<HobbyDto> Hobbies { get; set; }
+    }
+}
