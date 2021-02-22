e8393a8ce0df0e154882de3bb1df92e1371375c8
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-22 18:09:04 +0000

    Create Mediatr

---
+++ Next.Steps.Application/Query/PersonGetAllQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Next.Steps.Application.Query
+{
+    public class PersonGetAllQuery
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Next.Steps.Application/Query/PersonGetByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Next.Steps.Application.Query
+{
+    public class PersonGetByIdQuery
+    {
+        public int Id { get; set; }
+    }
+}
