63d3408c72ef9182ade3b320b3c3dcffad7040c5
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-22 22:29:05 +0000

    Delete modelBuilder

--- Next.Steps.Repository/Context/NextStepsContext.cs
+++ Next.Steps.Repository/Context/NextStepsContext.cs
@@ -23,47 +23,13 @@ namespace Next.Steps.Repository.Context
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-JN31U5B;
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-JN31U5B\SQLEXPRESS;
                 Initial Catalog=Next_Steps_Project;
                 Persist Security Info=True;
                 User id=sa;
                 Password=Password1994");
             }
             base.OnConfiguring(optionsBuilder);
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.HasDefaultSchema("nextSteps");
-
-            modelBuilder.Entity<Person>( entity =>
-            {
-                entity.HasKey(e => e.Id);
-                entity.Property(e => e.FirstName).IsRequired();
-                entity.Property(e => e.LastName).IsRequired();
-                entity.Property(e => e.Profession).IsRequired();
-                entity.Property(e => e.Birthdate).IsRequired();
-                entity.Property(e => e.Email).IsRequired();
-                entity.Property(e => e.Hobbies);
-            }
-            );
-        }
-
-        public override int SaveChanges()
-        {
-            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataSaves") != null))
-            {
-                if (entry.State == EntityState.Added)
-                {
-                    entry.Property("DataSaves").CurrentValue = DateTime.Now;
-                }
-
-                if (entry.State == EntityState.Modified)
-                {
-                    entry.Property("DataSaves").IsModified = false;
-                }
-            }
-            return base.SaveChanges();
         }
     }
 }
