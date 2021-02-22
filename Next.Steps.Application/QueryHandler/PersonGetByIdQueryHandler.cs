9f4aef43d2550a218f37ca6b8dc9ebab9d2f756a
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-22 22:12:07 +0000

    Create PersonGetByIdQueryHandler

---
+++ Next.Steps.Application/QueryHandler/PersonGetByIdQueryHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Next.Steps.Application.QueryHandler
+{
+    public class PersonGetByIdQueryHandler
+    {
+
+    }
+}
