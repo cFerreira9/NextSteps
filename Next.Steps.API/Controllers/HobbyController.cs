6dac6153fb8d6cd72e59ba275f1f8fe807a6f01a
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-22 23:30:00 +0000

    Create HobbyController

---
+++ Next.Steps.API/Controllers/HobbyController.cs
@@ -0,0 +1,81 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Next.Steps.Application.Command;
+using Next.Steps.Application.Dto;
+using Next.Steps.Application.Query;
+using System.Collections.Generic;
+
+namespace Next.Steps.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HobbyController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        private readonly IMediator _mediator;
+
+        public HobbyController(ILogger<PersonController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<HobbyDto>> GetAll()
+        {
+            var query = new HobbyGetAllQuery();
+
+            var response = _mediator.Send(query);
+
+            if (response == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(response);
+            }
+        }
+
+        [HttpPost]
+        public void Create(HobbyDto h)
+        {
+            var command = new HobbyCreateCommand
+            {
+                Hobby = h
+            };
+
+            var status = _mediator.Send(command);
+
+            Ok(status);
+        }
+
+        [HttpPut]
+        public void Put(HobbyDto h)
+        {
+            var command = new HobbyUpdateCommand
+            {
+                Hobby = h
+            };
+
+            var status = _mediator.Send(command);
+
+            Ok(status);
+        }
+
+        [HttpDelete]
+        public void Delete(HobbyDto h)
+        {
+            var command = new HobbyDeleteCommand
+            {
+                Hobby = h
+            };
+
+            var status = _mediator.Send(command);
+
+            Ok(status);
+        }
+    }
+}
