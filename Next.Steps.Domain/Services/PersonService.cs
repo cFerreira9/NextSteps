040184a0d0e0ed37db4c70601036387d5484a2ca
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-23 18:32:08 +0000

    Implement PersonService

--- Next.Steps.Domain/Services/PersonService.cs
+++ Next.Steps.Domain/Services/PersonService.cs
@@ -1,11 +1,18 @@
 ﻿using Next.Steps.Domain.Entities;
+using Next.Steps.Domain.Interfaces.Repositories;
 using Next.Steps.Domain.Interfaces.Services;
 using System;
 using System.Collections.Generic;
 
 namespace Next.Steps.Domain.Services
 {
-    internal class PersonService : BaseService<Person>, IPersonService
+    public class PersonService : BaseService<Person>, IPersonService
     {
+        private IPersonRepository _prepo;
+
+        public PersonService(IPersonRepository prepo) : base(prepo)
+        {
+            _prepo = prepo;
+        }
     }
 }
