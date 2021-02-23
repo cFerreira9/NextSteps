8c5560e590803365a8c77b55470dac2d9166dde1
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-23 18:33:19 +0000

    Create PersonRepository

---
+++ Next.Steps.Repository/Repository/PersonRepository.cs
@@ -0,0 +1,12 @@
+﻿using Next.Steps.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Next.Steps.Repository.EF.Repository
+{
+    //public class PersonRepository : BaseRepository<Person>
+    //{
+
+    //}
+}
