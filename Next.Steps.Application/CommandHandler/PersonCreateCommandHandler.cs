cf7bd1ecb212056b0e7755e9639d55f79e8ec31d
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-23 18:27:47 +0000

    Created PersonCreateCommandHandler

--- Next.Steps.Application/CommandHandler/PersonCreateCommandHandler.cs
+++ Next.Steps.Application/CommandHandler/PersonCreateCommandHandler.cs
@@ -1,6 +1,32 @@
-﻿namespace Next.Steps.Application.CommandHandler
+﻿using MediatR;
+using Next.Steps.Application.Command;
+using Next.Steps.Domain.Entities;
+using Next.Steps.Domain.Interfaces.Services;
+
+namespace Next.Steps.Application.CommandHandler
 {
-    public class PersonCreateCommandHandler
+    public class PersonCreateCommandHandler : RequestHandler<PersonCreateCommand>
     {
+        private IPersonService _service;
+
+        public PersonCreateCommandHandler(IPersonService service)
+        {
+            _service = service;
+        }
+
+        protected override void Handle(PersonCreateCommand request)
+        {
+            var x = new Person
+            {
+                FirstName = request.Person.FirstName,
+                LastName = request.Person.LastName,
+                Profession = request.Person.Profession,
+                Birthdate = request.Person.Birthdate,
+                Email = request.Person.Email,
+                Hobbies = request.Person.Hobbies
+            };
+
+            _service.Create(x);
+        }
     }
 }
