b3215ca3062a1a18e464751518a88859640086ae
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-24 11:04:48 +0000

    Add HobbyDto

---
+++ Next.Steps.Application/Dto/HobbyDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Next.Steps.Application.Dto
+{
+    public class HobbyDto
+    {
+        public int Id { get; set; }
+
+        public int PersonId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+    }
+}
