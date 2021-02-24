4c599b795e1352a0acce943c923497ea10a6b77f
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-24 11:16:53 +0000

    Add PersonReadDto

---
+++ Next.Steps.Application/Dto/PersonReadDto.cs
@@ -0,0 +1,25 @@
+﻿using Next.Steps.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Next.Steps.Application.Dto
+{
+    public class PersonReadDto
+    {
+        [Required(ErrorMessage = "Person's Id is Required")]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Profession { get; set; }
+
+        public DateTime? Birthdate { get; set; }
+
+        public string Email { get; set; }
+
+        public virtual IEnumerable<HobbyDto> Hobbies { get; set; }
+    }
+}
