a224afedcc9ddfe3c5ba388413cab914e914923d
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-24 15:48:20 +0000

    Build Create method on FakeRepo

--- Next.Steps.Repository.Fake/FakeRepo.cs
+++ Next.Steps.Repository.Fake/FakeRepo.cs
@@ -6,27 +6,41 @@ namespace Next.Steps.Repository.Fake
 {
     public class FakeRepo : IPersonRepository
     {
-        private List<Person> list = new List<Person>();
+        private static List<Person> personList = new List<Person>();
+
+        //private static List<Hobby> hobbyList = new List<Hobby>();
+
+        private static int personId = 1;
+
+        private static int hobbyId = 1;
 
         public bool Create(Person p)
         {
-            var index = list.IndexOf(p);
-            if (index != -1)
+            p.Id = personId;
+
+            var index = personList.IndexOf(p);
+            if (index < 0)
             {
-                list.Add(p);
+                foreach (var item in p.Hobbies)
+                {
+                    item.Id = hobbyId;
+                    hobbyId++;
+                }
+
+                personList.Add(p);
+                personId++;
                 return true;
             }
-
             return false;
         }
 
         public bool Update(Person p)
         {
-            var index = list.IndexOf(p);
+            var index = personList.FindIndex(x => x.Id == p.Id);
 
             if (index >= 0)
             {
-                list[index] = p;
+                personList[index] = p;
                 return true;
             }
 
@@ -35,11 +49,11 @@ namespace Next.Steps.Repository.Fake
 
         public bool Delete(Person p)
         {
-            var index = list.IndexOf(p);
+            var index = personList.IndexOf(p);
 
             if (index >= 0)
             {
-                list.Remove(p);
+                personList.Remove(p);
                 return true;
             }
 
@@ -48,15 +62,15 @@ namespace Next.Steps.Repository.Fake
 
         public IEnumerable<Person> GetAll()
         {
-            return list;
+            return personList;
         }
 
         public Person GetByID(int id)
         {
-            var index = list.FindIndex(l => l.Id == id);
+            var index = personList.FindIndex(l => l.Id == id);
             if (index >= 0)
             {
-                return list[index];
+                return personList[index];
             }
             else
             {
@@ -66,7 +80,7 @@ namespace Next.Steps.Repository.Fake
 
         public IEnumerable<Person> Search(string firstName, string lastName = "")
         {
-            return list.FindAll(l => l.FirstName == firstName || (lastName != "" && l.LastName == lastName));
+            return personList.FindAll(l => l.FirstName == firstName || (lastName != "" && l.LastName == lastName));
         }
     }
 }
