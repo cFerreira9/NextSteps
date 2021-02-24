d32e9a123793a9e04c3ec823e289a51fd6ec7bec
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-24 14:48:30 +0000

    Build PersonUpdateCommandHandler

--- Next.Steps.Application/CommandHandler/PersonUpdateCommandHandler.cs
+++ Next.Steps.Application/CommandHandler/PersonUpdateCommandHandler.cs
@@ -1,6 +1,46 @@
-﻿namespace Next.Steps.Application.CommandHandler
+﻿using MediatR;
+using Next.Steps.Application.Command;
+using Next.Steps.Domain.Entities;
+using Next.Steps.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+
+namespace Next.Steps.Application.CommandHandler
 {
-    public class PersonUpdateCommandHandler
+    public class PersonUpdateCommandHandler : RequestHandler<PersonUpdateCommand>
     {
+        private IPersonService _service;
+
+        public PersonUpdateCommandHandler(IPersonService service)
+        {
+            _service = service;
+        }
+
+        protected override void Handle(PersonUpdateCommand request)
+        {
+            var hobList = new List<Hobby>();
+
+            foreach (var item in request.Person.Hobbies)
+            {
+                hobList.Add(new Hobby
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    Type = item.Type
+                });
+            }
+
+            var person = new Person
+            {
+                Id = request.Person.Id,
+                FirstName = request.Person.FirstName,
+                LastName = request.Person.LastName,
+                Birthdate = (DateTime)request.Person.Birthdate,
+                Email = request.Person.Email,
+                Hobbies = hobList
+            };
+
+            _service.Update(person);
+        }
     }
 }
