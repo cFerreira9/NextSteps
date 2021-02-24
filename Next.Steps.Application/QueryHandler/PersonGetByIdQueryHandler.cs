e530accd15331670341cb6983de173769ad45f4c
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-24 11:25:27 +0000

    PersonGetByIdQueryHandler back to create state

--- Next.Steps.Application/QueryHandler/PersonGetByIdQueryHandler.cs
+++ Next.Steps.Application/QueryHandler/PersonGetByIdQueryHandler.cs
@@ -1,15 +1,15 @@
 ﻿using MediatR;
+using Next.Steps.Application.Dto;
 using Next.Steps.Application.Query;
+using Next.Steps.Domain.Entities;
+using Next.Steps.Domain.Interfaces.Services;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Next.Steps.Application.QueryHandler
 {
-    public class PersonGetByIdQueryHandler : IRequestHandler<PersonGetByIdQuery, string>
+    public class PersonGetByIdQueryHandler
     {
-        public Task<string> Handle(PersonGetByIdQuery request, CancellationToken cancellationToken)
-        {
-            return Task.FromResult("Pong");
-        }
+
     }
 }
