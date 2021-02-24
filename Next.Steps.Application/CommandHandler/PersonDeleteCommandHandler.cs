0b2756f4769f4785f6ad7d52840fc7320b49e112
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-24 11:04:30 +0000

    Add PersonDeleteCommandHandler funcionality

--- Next.Steps.Application/CommandHandler/PersonDeleteCommandHandler.cs
+++ Next.Steps.Application/CommandHandler/PersonDeleteCommandHandler.cs
@@ -1,6 +1,24 @@
-﻿namespace Next.Steps.Application.CommandHandler
+﻿using MediatR;
+using Next.Steps.Application.Command;
+using Next.Steps.Application.Dto;
+using Next.Steps.Domain.Interfaces.Services;
+
+namespace Next.Steps.Application.CommandHandler
 {
-    public class PersonDeleteCommandHandler
+    public class PersonDeleteCommandHandler : RequestHandler<PersonDeleteCommand>
     {
+        private IPersonService _service;
+
+        public PersonDeleteCommandHandler(IPersonService service)
+        {
+            _service = service;
+        }
+
+        protected override void Handle(PersonDeleteCommand request)
+        {
+            
+
+            _service.Delete();
+        }
     }
 }
