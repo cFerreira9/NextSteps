0557584d3469f495a13f936d7b326b1795958af0
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-24 11:17:06 +0000

    Add PersonWriteDto

---
+++ Next.Steps.Application/Dto/PersonWriteDto.cs
@@ -0,0 +1,29 @@
+﻿using Next.Steps.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Next.Steps.Application.Dto
+{
+    public class PersonWriteDto
+    {
+        [Required(ErrorMessage = "Person's Firstname is Required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Person's Lastname is Required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Person's Profession is Required")]
+        public string Profession { get; set; }
+
+        [Required(ErrorMessage = "Person's Birthday is Required")]
+        public DateTime? Birthdate { get; set; }
+
+        [Required(ErrorMessage = "Person's Email is Required")]
+        [EmailAddress(ErrorMessage = "Invalid Email")]
+        public string Email { get; set; }
+
+        public virtual IEnumerable<HobbyDto> Hobbies { get; set; }
+    }
+}
