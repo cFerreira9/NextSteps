97337b22410efd1f994a6ef3a6079799edd9949d
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-25 14:18:53 +0000

    Add properties on PersonSearchQuery

--- Next.Steps.Application/Query/PersonSearchQuery.cs
+++ Next.Steps.Application/Query/PersonSearchQuery.cs
@@ -1,9 +1,13 @@
 ﻿using MediatR;
 using Next.Steps.Application.Dto;
+using System.Collections.Generic;
 
 namespace Next.Steps.Application.Query
 {
-    public class PersonSearchQuery : IRequest<PersonReadDto>
+    public class PersonSearchQuery : IRequest<IEnumerable<PersonReadDto>>
     {
+        public string Firstname;
+
+        public string Lastname;
     }
 }
