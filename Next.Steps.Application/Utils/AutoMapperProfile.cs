e3729bd99a0b8f18e4acd25ee3779a83f07a78ae
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-25 12:13:06 +0000

    Add AutoMapper

---
+++ Next.Steps.Application/Utils/AutoMapperProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Next.Steps.Application.Dto;
+using Next.Steps.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Next.Steps.Application.Utils
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Person, PersonReadDto>()
+                .ForMember(p => p.Birthdate, opt =>
+                opt.MapFrom(src => src.Birthdate.ToString("yyyy-MM-dd")));
+
+            CreateMap<PersonWriteDto, Person>()
+                .ForMember(p => p.Id, opt => opt.Ignore())
+                .ForMember(p => p.Birthdate, opt =>
+                opt.MapFrom(src => (DateTime)src.Birthdate));
+
+            CreateMap<Hobby, HobbyDto>().ReverseMap();
+            CreateMap<HobbyDto, Hobby>();
+        }
+    }
+}
