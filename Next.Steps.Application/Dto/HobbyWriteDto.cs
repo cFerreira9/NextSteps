8388b76ce38fc22801c17ad4c6242244125f13e1
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-26 10:36:31 +0000

    Create HobbyWriteDto

---
+++ Next.Steps.Application/Dto/HobbyWriteDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Next.Steps.Application.Dto
+{
+    public class HobbyWriteDto
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+    }
+}
