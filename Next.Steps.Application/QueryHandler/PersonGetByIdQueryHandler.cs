59d5cb736badebbb00c4941ee089bdc5816b5a29
Carlos Ferreira <filipee-94@hotmail.com>
2021-02-26 10:05:08 +0000

    Clean unuse usings

--- Next.Steps.Application/QueryHandler/PersonGetByIdQueryHandler.cs
+++ Next.Steps.Application/QueryHandler/PersonGetByIdQueryHandler.cs
@@ -4,9 +4,6 @@ using Next.Steps.Application.Dto;
 using Next.Steps.Application.Query;
 using Next.Steps.Domain.Entities;
 using Next.Steps.Domain.Interfaces.Services;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Next.Steps.Application.QueryHandler
 {
