2738c5e18cfdc1c6f3ca32271a783cec07f15f0d
Carlos Ferreira <filipee-94@hotmail.com>
2021-03-08 11:22:33 +0000

    Add Samples in Actions

--- Next.Steps.API/Controllers/PersonController.cs
+++ Next.Steps.API/Controllers/PersonController.cs
@@ -22,8 +22,16 @@ namespace Next.Steps.API.Controllers
         /// <summary>
         /// Get All People
         /// </summary>
+        /// <remarks>
+        /// Sample:
+        ///
+        ///     Get /Person
+        ///
+        /// </remarks>
         /// <returns></returns>
+        /// <response code="200">Success to get all people.</response>
         [HttpGet]
+        [ProducesResponseType(200)]
         public async Task<IActionResult> GetAllAsync()
         {
             var query = new PersonGetAllQuery();
@@ -37,9 +45,21 @@ namespace Next.Steps.API.Controllers
         /// <summary>
         /// Get Person by Id
         /// </summary>
+        /// <remarks>
+        /// Sample:
+        ///
+        ///     Get /Person/1
+        ///
+        /// </remarks>
         /// <param name="id"></param>
         /// <returns></returns>
+        /// <response code="200">Success to get the person by the Id.</response>
+        /// <response code="400">Invalid Id! The Id must be greater than 0.</response>
+        /// <response code="404">Not found! There's no one with that id.</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
         public async Task<IActionResult> GetByIdAsync(int id)
         {
             if (id <= 0)
@@ -62,9 +82,33 @@ namespace Next.Steps.API.Controllers
         /// <summary>
         /// Create Person
         /// </summary>
+        /// <remarks>
+        /// Sample:
+        ///
+        ///     Post /Person
+        ///
+        ///     {
+        ///         "firstName": "Carlos",
+        ///         "lastName": "Ferreira",
+        ///         "profession": "Developer",
+        ///         "birthdate": "1999-05-08",
+        ///         "email": "carlos.ferreira@decode.pt",
+        ///         "hobbies": [
+        ///             {
+        ///                 "name": "Pescar",
+        ///                 "type": "Desporto Radical"
+        ///             }
+        ///         ]
+        ///     }
+        ///
+        /// </remarks>
         /// <param name="p">Pessoa</param>
         /// <returns></returns>
+        /// <response code="200">Success to create the person.</response>
+        /// <response code="400">Couldn't create! Invalid parameters! Check the example.</response>
         [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
         public async Task<IActionResult> CreateAsync(PersonWriteDto p)
         {
             if (p == null)
@@ -87,9 +131,36 @@ namespace Next.Steps.API.Controllers
         /// <summary>
         /// Update Person
         /// </summary>
+        /// <remarks>
+        /// Sample:
+        ///
+        ///     Update /Person
+        ///
+        ///     {
+        ///         "Id": "1",
+        ///         "firstName": "Carlos",
+        ///         "lastName": "Ferreira",
+        ///         "profession": "Developer",
+        ///         "birthdate": "1999-05-08",
+        ///         "email": "carlos.ferreira@decode.pt",
+        ///         "hobbies": [
+        ///             {
+        ///                 "name": "Pescar",
+        ///                 "type": "Desporto Radical"
+        ///             }
+        ///         ]
+        ///     }
+        ///
+        /// </remarks>
         /// <param name="p"></param>
         /// <returns></returns>
+        /// <response code="200">Success to create the person.</response>
+        /// <response code="400">Couldn't create! Invalid parameters! Check the example.</response>
+        /// <response code="404">Not found! There's no one with that id.</response>
         [HttpPut]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
         public async Task<IActionResult> PutAsync(PersonUpdateDto p)
         {
             if (p == null)
@@ -112,9 +183,21 @@ namespace Next.Steps.API.Controllers
         /// <summary>
         /// Delete Person
         /// </summary>
+        /// <remarks>
+        /// Sample:
+        ///
+        ///     Delete /Person/1
+        ///
+        /// </remarks>
         /// <param name="id"></param>
         /// <returns></returns>
+        /// <response code="200">Success to delete the person.</response>
+        /// <response code="400">Couldn't delete! Invalid Id! Check the Id, must be greater than 0.</response>
+        /// <response code="404">Not found! There's no one with that id.</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
         public async Task<IActionResult> DeleteAsync(int id)
         {
             if (id <= 0)
@@ -137,10 +220,22 @@ namespace Next.Steps.API.Controllers
         /// <summary>
         /// Search person by firstname or lastname
         /// </summary>
+        /// <remarks>
+        /// Sample:
+        ///
+        ///     Get /Search
+        ///
+        /// </remarks>
         /// <param name="firstName">Firstname to search</param>
         /// <param name="lastName">Lastname to search</param>
         /// <returns></returns>
+        /// <response code="200">Success to search the person by firstname or lastname.</response>
+        /// <response code="400">Couldn't search! Invalid firstname or lastname! Check the parametes, must be a valid name.</response>
+        /// <response code="404">Not found! There's no one with that firstname or lastname.</response>
         [HttpGet("/[Action]")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
         public async Task<IActionResult> SearchAsync(string firstName, string lastName)
         {
             var query = new PersonSearchQuery
